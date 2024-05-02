5dce4e5478644a74cf7221c1552e6a6bea9174a5
dowellgandy <dowell.gandy@gmail.com>
2024-05-02 14:33:38 -0400

    Thru Ch13 working

--- settings.py
+++ settings.py
@@ -10,10 +10,17 @@ class Settings:
 
         # Ship Settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # Bullet Settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (200, 200, 20)
         self.bullets_allowed = 5
+
+        # Alien Settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
